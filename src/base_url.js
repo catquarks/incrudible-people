60e7ecd2e34dca60eca995c97e2aa041b9565b4f
Tami <catquarks@gmail.com>
2017-03-25 22:24:45 -0400

    change baseUrl back to heroku API

--- src/base_url.js
+++ src/base_url.js
@@ -1,4 +1,3 @@
-const baseUrl = 'http://localhost:3000/api/v1'
-// const baseUrl = 'https://coolpeople.herokuapp.com/api/v1'
+const baseUrl = 'https://coolpeople.herokuapp.com/api/v1'
 
 export default baseUrl
