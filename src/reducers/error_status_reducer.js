484da3e2d9a632f644e01fd660e733dad4c760f2
Tami <catquarks@gmail.com>
2017-03-29 18:57:18 -0400

    TOGGLE_ERROR_STATUS action type now returns true RESET_ERROR_STATUS already returns false

--- src/reducers/error_status_reducer.js
+++ src/reducers/error_status_reducer.js
@@ -1,7 +1,7 @@
 export default function ErrorStatusReducer(state=false, action) {
 	switch(action.type) {
 		case 'TOGGLE_ERROR_STATUS':
-			return !state
+			return true
 		case 'RESET_ERROR_STATUS':
 			return false
 		default:
